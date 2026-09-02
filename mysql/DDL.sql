7e700e364a214eb063918aade6cb7a59fea7b6f5
Felipe Agudelo <felipeagudeloolaya@gmail.com>
2026-09-02 16:56:06 -0500

    Updates

--- mysql/DDL.sql
+++ mysql/DDL.sql
@@ -1,4 +1,14 @@
 -----------------> DDL (Data Definition Language) <-----------------
+/*
+Lenguaje para definir la estructura de la base de datos.
+
+Se usa para:
+- crear bases de datos
+- crear tablas
+- modificar tablas
+- eliminar estructuras
+*/
+
 
 ----------------------------------------------------------------------
 -- BASES DE DATOS
@@ -9,10 +19,10 @@
 CREATE DATABASE sonsultas_sql;
 
 -- 1.1 DROP
--- Eliminar una base de datos
+-- Eliminar completamente una base de datos y todo su contenido
 DROP DATABASE consultas_sql;
 
--- Seleccionar una base de datos para trabajar
+-- 1.2 Seleccionar la base de datos con la que se va a trabajar
 USE consultas_sql;
 
 
@@ -21,7 +31,7 @@ USE consultas_sql;
 ----------------------------------------------------------------------
 
 -- 2.0 CREATE
--- Crear tablas
+-- Crear una tabla llamada productos con diferentes tipos de datos
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(55) NOT NULL,
@@ -29,29 +39,97 @@ CREATE TABLE productos (
     stock INT DEFAULT 0
 );
 
+-- 2.1 Asignar una FOREIGN KEY
+CREATE TABLE compras(
+	id_compra INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_compra DATE NOT NULL,
+    precio_compra DOUBLE NOT NULL,
+    id_cliente INT,
+    id_producto INT,
+    
+    CONSTRAINT fk_compra_cliente
+    FOREIGN KEY (id_cliente)
+    REFERENCES clientes(id_cliente)
+    ON DELETE RESTRICT, -- Bloquea el borrado
+
+    CONSTRAINT fk_compra_producto
+    FOREIGN KEY (id_producto) 
+    REFERENCES productos(id_producto)
+    ON DELETE CASCADE  --Borra todo en cadena
+    ON UPDATE CASCADE, --Actualiza todo en cadena
+);
+
+----------------------------------------------------------------------
+-- RENAME
+----------------------------------------------------------------------
+-- 2.2 Renombrar una tabla
+RENAME TABLE productos TO articulos;
+
 ----------------------------------------------------------------------
 -- ALTER TABLE
 ----------------------------------------------------------------------
--- 2.1 Agregar una nueva columna
-ALTER TABLE productos ADD descripcion VARCHAR(150) NOT NULL;
+-- 2.3 Agregar una nueva columna
+ALTER TABLE productos 
+ADD descripcion VARCHAR(150) NOT NULL;
 
--- 2.2 Renombrar una columna (MySQL 8+)
-ALTER TABLE productos RENAME COLUMN precio TO costo;
+-- 2.4 Agregar una nueva columna como id
+ALTER TABLE productos
+ADD id_producto INT;
 
--- 2.3 Renombrar una columna (método alternativo)
-ALTER TABLE productos CHANGE precio costo DOUBLE;
+-- 2.5 Asignar una PRIMARY KEY
+ALTER TABLE productos
+ADD PRIMARY KEY (id_producto);
 
--- 2.4 Modificar tipo de dato 12:Enteros 2:Decimales
-ALTER TABLE productos MODIFY costo DECIMAL(12,2);
+-- 2.6 Modificar una PRIMARY KEY para que sea AUTO_INCREMENT
+ALTER TABLE productos
+MODIFY id_producto INT AUTO_INCREMENT;
 
--- 2.5 Eliminar una columna
-ALTER TABLE productos DROP COLUMN descripcion;
+-- 2.7 Agregar una PRIMARY KEY
+ALTER TABLE productos
+ADD id_producto INT AUTO_INCREMENT PRIMARY KEY;
+
+-- 2.8 Asignar una FOREIGN KEY
+ALTER TABLE productos
+ADD CONSTRAINT fk_productos_grupo
+FOREIGN KEY (id_grupo) 
+REFERENCES grupos(id_grupo)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- 2.9 Renombrar una columna (MySQL 8+)
+ALTER TABLE productos 
+RENAME COLUMN precio TO costo;
+
+-- 2.10 Renombrar una columna (método alternativo)
+ALTER TABLE productos 
+CHANGE precio costo DOUBLE;
+
+-- 2.11 Modificar el tipo de dato de una columna
+-- DECIMAL(12,2) significa:
+-- 12 dígitos totales, 2 después del punto decimal
+ALTER TABLE productos 
+MODIFY costo DECIMAL(12,2);
+
+-- 2.12 Eliminar una columna
+ALTER TABLE productos 
+DROP COLUMN descripcion;
+
+-- 2.13 Eliminar una FOREIGN KEY
+ALTER TABLE productos
+DROP FOREIGN KEY productos_ibfk_1;
+
+----------------------------------------------------------------------
+-- SHOW
+----------------------------------------------------------------------
+-- Cómo saber el nombre de la FOREIGN KEY
+SHOW CREATE TABLE productos;
 
 ----------------------------------------------------------------------
 -- ELIMINAR TABLAS
 ----------------------------------------------------------------------
--- 2.6 Eliminar una tabla
+-- 2.14 Eliminar una tabla completamente
 DROP TABLE productos;
 
--- 2.7 Vaciar una tabla sin eliminar su estructura
+-- 2.15 Vaciar todos los registros de la tabla
+-- La estructura se mantiene
 TRUNCATE TABLE productos;
--- mysql/DML.sql
+++ mysql/DML.sql
@@ -1,59 +1,69 @@
 -----------------> DML (Data Manipulation Language) <-----------------
+/*
+Lenguaje para manipular los datos dentro de las tablas.
+
+Se usa para:
+- insertar datos
+- consultar datos
+- actualizar datos
+- eliminar datos
+*/
+
 
 ----------------------------------------------------------------------
 -- INSERT
 ----------------------------------------------------------------------
--- 1 Insertar un registro
+-- 1 Insertar un solo registro en la tabla
 INSERT INTO productos (nombre, precio, stock)
 VALUES ('Arroz', 3500, 50);
 
--- 2 Insertar múltiples registros
+-- 2 Insertar múltiples registros en una sola consulta
 INSERT INTO productos (nombre, precio, stock) VALUES
 ('Arroz', 3500, 50),
 ('Frijoles', 4200, 35),
 ('Lentejas', 3800, 40);
 
 ----------------------------------------------------------------------
--- SELECT
+-- SELECT (Consultas)
 ----------------------------------------------------------------------
--- 3 Mostrar todos los productos
+-- 3 Mostrar todos los registros de la tabla
 SELECT * 
 FROM productos;
 
 -- 4 Mostrar productos con precio mayor o igual a 4000
 SELECT nombre, precio, stock 
 FROM productos 
-WHERE (precio >= 4000)
+WHERE (precio >= 4000);
 
 -- 5 Mostrar productos con precio >= 2000 y stock >= 25
 SELECT nombre, precio, stock 
 FROM productos 
 WHERE (precio >= 4000) AND (stock >= 25);
 
--- 6 Mostrar 
+-- 6 Mostrar productos cuyos ID estén en la lista 
 SELECT *
 FROM productos
 WHERE id IN (1, 2);
 
--- 7 Mostrar 
+-- 7 Mostrar productos cuyo nombre esté en la lista 
 SELECT *
 FROM productos
 WHERE nombre IN ('Queso', 'Carne');
 
 ----------------------------------------------------------------------
--- UPDATE
+-- UPDATE (Actualizar, Modificar)
 ----------------------------------------------------------------------
--- 8 Actualizar de la tabla productos el precio en 3500 donde el id sea 4
+-- 8 Actualizar el precio del producto cuyo id sea 4
 UPDATE productos
 SET precio = 3500
 WHERE id = 4;
 
--- 9 Actualizar de la tabla productos el precio en 3500 donde el nombre sea 'Azúcar'
+-- 9 Actualizar el precio del producto llamado 'Azúcar'
 UPDATE productos
 SET precio = 3500
 WHERE nombre = 'Azúcar';
 
--- 10 Actualizar el nombre de un producto
+-- 10 Cambiar el nombre de un producto
 UPDATE productos
 SET nombre = 'Arroz Diana'
 WHERE nombre = 'Arroz Roa';
@@ -61,87 +71,94 @@ WHERE nombre = 'Arroz Roa';
 ----------------------------------------------------------------------
 -- DELETE
 ----------------------------------------------------------------------
--- 11 Borrar
+-- 11 Eliminar un registro por id
 DELETE FROM productos
 WHERE id = 4;
 
--- 12 Borrar
+-- 12 Eliminar varios registros usando IN
 DELETE FROM productos
 WHERE id IN (1, 2);
 
--- 13 Borrar
+-- 13 Eliminar registros por nombre
 DELETE FROM productos
 WHERE nombre IN ('Queso', 'Carne');
 
 ----------------------------------------------------------------------
--- ORDER BY
+-- ORDER BY (Ordenar resultados)
 ----------------------------------------------------------------------
--- 14 Ordenar por nombre Ascendente
+-- 14 Ordenar productos por nombre (ascendente)
 SELECT *
 FROM productos
 ORDER BY nombre ASC;
 
--- 15 Ordenar por nombre Descendente
+-- 15 Ordenar productos por nombre (descendente)
 SELECT *
 FROM productos
 ORDER BY nombre DESC;
 
--- 16 Ordenar por precio Ascendente
+-- 16 Ordenar productos por precio (ascendente)
 SELECT *
 FROM productos
 ORDER BY precio ASC;
 
--- 17 Ordenar por precio Descendente
+-- 17 Ordenar productos por precio (descendente)
 SELECT *
 FROM productos
 ORDER BY precio DESC;
 
+
 ----------------------------------------------------------------------
--- COUNT
+-- FUNCIONES DE AGREGACIÓN
+
+-- Estas funciones trabajan sobre conjuntos de filas.
 ----------------------------------------------------------------------
--- 18 Contar todos los productos de la tabla productos
+
+----------------------------------------------------------------------
+-- COUNT (Contar registros)
+----------------------------------------------------------------------
+-- 18 Contar todos los registros de la tabla
 SELECT COUNT(*)
 FROM productos;
 
--- 19 Contar todos los productos de la tabla productos y guardarlos en una variable
+-- 19 Contar registros y asignar alias al resultado
 SELECT COUNT(*) AS cant_productos
 FROM productos;
 
 ----------------------------------------------------------------------
--- SUM
+-- SUM (Sumar valores numéricos)
 ----------------------------------------------------------------------
--- 20 Sumar el stock de la tabla productos
+-- 20 Sumar todo el stock disponible
 SELECT SUM(stock)
 FROM productos;
 
--- 21 Sumar el stock de la tabla productos donde el stock sea >= 50
+-- 21 Sumar el stock donde el stock sea mayor o igual a 50
 SELECT SUM(stock)
 FROM productos
 WHERE stock >= 50;
 
--- 22 Sumar el stock de la tabla productos y guardarlos en una variable
+-- 22 Sumar el stock y guardar el resultado con alias
 SELECT SUM(stock) AS sum_stock
 FROM productos;
 
 ----------------------------------------------------------------------
 -- MIN Y MAX
 ----------------------------------------------------------------------
--- 23 Mostrar el precio Minimo de los productos
+-- 23 Obtener el precio mínimo
 SELECT MIN(precio)
 FROM productos;
 
--- 24 Mostrar el precio Maximo de los productos
+-- 24 Obtener el precio máximo
 SELECT MAX(precio)
 FROM productos;
 
 ----------------------------------------------------------------------
--- AVERAGE - (Calcular el promedio de valores numericos)
+-- AVG (Average - Promedio / Calcular el promedio de valores numericos)
 ----------------------------------------------------------------------
--- 25 Mostrar el promedio del precio de los productos
+-- 25 Calcular el precio promedio de los productos
 SELECT AVG(precio)
 FROM productos;
 
--- 26 Mostrar el promedio del precio de los productos con precio >= 4000
+-- 26 Promedio del precio donde el precio >= 4000
 SELECT AVG(precio)
 FROM productos
 WHERE precio >= 4000;
@@ -149,55 +166,114 @@ WHERE precio >= 4000;
 ----------------------------------------------------------------------
 -- DISTINCT (Para no mostrar los valores repetidos)
 ----------------------------------------------------------------------
--- 27 Mostrar los nombres sin repetir
+-- 27 Mostrar nombres de productos sin repetir
 SELECT DISTINCT nombre
 FROM productos;
+
+-- 28 Mostrar nombres de productos sin repetir y ordenarlo por nombre ASC
+SELECT DISTINCT nombre
+FROM productos
+ORDER BY nombre ASC;
+
+----------------------------------------------------------------------
+-- LIMIT (Para limitar la consulta)
+----------------------------------------------------------------------
+-- 29 Mostrar los 5 productos más baratos
+SELECT *
+FROM productos
+ORDER BY precio ASC
+LIMIT 5;
 
 ----------------------------------------------------------------------
 -- GROUP BY (Agrupar por columnas Ej. Varios tipos de Arroz)
 ----------------------------------------------------------------------
--- 28 Agrupar por los nombres de productos repetidos y
+-- 30 Agrupar productos por nombre y calcular estadísticas
 SELECT nombre, AVG(precio), SUM(precio), SUM(stock)
 FROM productos
 GROUP BY nombre;
 
--- 29
+-- 31 Agrupar productos caros
 SELECT nombre, SUM(precio), SUM(stock)
 FROM productos
 WHERE precio >= 9000
 GROUP BY nombre;
 
--- 30
+-- 32 Mostrar cantidad de registros por producto
 SELECT nombre, COUNT(*), SUM(precio), sum(stock)
 FROM productos
 GROUP BY nombre;
 
 ----------------------------------------------------------------------
 -- HAVING (Es como un 'WHERE' pero se usa para 'GROUP BY')
+-- filtra resultados después de aplicar GROUP BY
 ----------------------------------------------------------------------
--- 31
+-- 33 Mostrar productos que aparecen 3 o más veces
 SELECT nombre, COUNT(*), SUM(precio), sum(stock)
 FROM productos
 GROUP BY nombre
 HAVING COUNT(*) >= 3;
 
--- 32
+-- 34 Mostrar productos cuyo precio total sea mayor a 10000
 SELECT nombre, SUM(precio), SUM(stock)
 FROM productos
 GROUP BY nombre
 HAVING SUM(precio) >= 10000;
 
--- 33
+-- 35 Combinar WHERE + GROUP BY + HAVING
 SELECT nombre, SUM(precio), SUM(stock)
 FROM productos
 WHERE precio >= 9000
 GROUP BY nombre
 HAVING SUM(precio) >= 15000;
 
--- 34
+-- 36 Filtrar grupos por cantidad total de stock
 SELECT nombre, SUM(precio), SUM(stock)
 FROM productos
 WHERE precio >= 9000
 GROUP BY nombre
 HAVING SUM(stock) >= 25;
 
+
+
+
+----------------------------------------------------------------------
+-- INNER JOIN (Unir los datos que tengan un criterio en comun)
+----------------------------------------------------------------------
+-- Explicito
+SELECT *
+FROM empleados
+INNER JOIN departamentos
+ON empleados.id_departamento = departamentos.id_departamento;
+
+-- Implicito
+SELECT *
+FROM empleados, departamentos
+WHERE empleados.id_departamento = departamentos.id_departamento;
+
+-- Ejemplo con variables
+SELECT e.nombre, m.fecha, m.costo
+FROM equipos e
+INNER JOIN mantenimientos m
+ON e.id_equipo = m.id_equipo;
+
+
+----------------------------------------------------------------------
+-- LEFT JOIN 
+-- (Unir los registros de la tabla A con los registros 
+-- en comun de la tabla B)
+----------------------------------------------------------------------
+SELECT *
+FROM empleados
+LEFT JOIN departamentos
+ON empleados.id_departamento = departamentos.id_departamento;
+
+
+----------------------------------------------------------------------
+-- LEFT JOIN 
+-- (Unir los registros de la tabla B con los registros 
+-- en comun de la tabla A)
+----------------------------------------------------------------------
+SELECT *
+FROM empleados
+RIGHT JOIN departamentos
+ON empleados.id_departamento = departamentos.id_departamento;
